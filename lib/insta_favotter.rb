94d6625547341b5b0bea705dc5e431613239bd35
joe <kinjoyuji@gmail.com>
2019-02-14 15:39:04 +0900

    instagramへloginする実装

--- lib/insta_favotter.rb
+++ lib/insta_favotter.rb
@@ -1,8 +1,27 @@
 require "insta_favotter/version"
+require 'selenium-webdriver'
 
 module InstaFavotter
   class Error < StandardError; end
-  def self.greet
-    'Hello World!'
+
+  class Client
+    attr_reader :username, :password, :driver
+
+    def initialize(username, password)
+      @username = username
+      @password = password
+      @driver = Selenium::WebDriver.for(:chrome)
+    end
+
+    def login
+      driver.get "https://www.instagram.com/accounts/login/?source=auth_switcher"
+
+      Selenium::WebDriver::Wait.new(timeout: 10)
+
+      driver.find_element(:name, 'username').send_keys(username)
+      driver.find_element(:name, 'password').send_keys(password)
+
+      driver.find_elements(:tag_name, 'button').each{ |el| el.click }
+    end
   end
 end
