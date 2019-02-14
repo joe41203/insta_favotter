e3643392938416e61f1a20ce0f9675b58eb98dab
joe <kinjoyuji@gmail.com>
2019-02-14 20:52:10 +0900

    likeするまで実装

--- lib/insta_favotter.rb
+++ lib/insta_favotter.rb
@@ -1,27 +1,61 @@
-require "insta_favotter/version"
+require 'insta_favotter/version'
 require 'selenium-webdriver'
 
 module InstaFavotter
   class Error < StandardError; end
 
   class Client
+    LOGIN_SUBMIT_NUMBER = 1.freeze
+
     attr_reader :username, :password, :driver
 
     def initialize(username, password)
       @username = username
       @password = password
-      @driver = Selenium::WebDriver.for(:chrome)
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => {
+            'args' => ['--user-data-dir=./profile']
+              })
+      @driver = Selenium::WebDriver.for :chrome , desired_capabilities: caps
     end
 
     def login
-      driver.get "https://www.instagram.com/accounts/login/?source=auth_switcher"
+      driver.get 'https://www.instagram.com/accounts/login/?source=auth_switcher'
 
-      Selenium::WebDriver::Wait.new(timeout: 10)
+      Selenium::WebDriver::Wait.new(timeout: 100)
+
+      return if logined?
 
       driver.find_element(:name, 'username').send_keys(username)
       driver.find_element(:name, 'password').send_keys(password)
 
-      driver.find_elements(:tag_name, 'button').each{ |el| el.click }
+      Selenium::WebDriver::Wait.new(timeout: 100)
+
+      driver.find_elements(:tag_name, 'button')[LOGIN_SUBMIT_NUMBER].click
+      
+      self
+    end
+
+    def search(tag_name)
+      driver.get "https://www.instagram.com/explore/tags/#{tag_name}/"
+    end
+
+    def first_el_click
+      driver.find_element(:xpath, "//*[@id='react-root']/section/main/article/div[2]/div/div[1]/div[1]/a/div/div[2]").click
+    end
+
+    def fav
+      driver.find_element(:xpath, "/html/body/div[2]/div[2]/div/article/div[2]/section[1]/span[1]/button/span").click
+    end
+
+    def next_pic
+      driver.find_element(:xpath, "/html/body/div[2]/div[1]/div/div/a[2]").click
+    end
+
+    private
+
+    def logined?
+      driver.current_url == 'https://www.instagram.com/'
     end
   end
 end
